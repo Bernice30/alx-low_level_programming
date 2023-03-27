551af67440983a0bf3666bb8519d16b1ca344dd2
Bernice30 <maureenotono@gmail.com>
2023-03-27 11:15:49 -0700

    printf program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main funcion of the code
+ *
+ * Return: this is always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
