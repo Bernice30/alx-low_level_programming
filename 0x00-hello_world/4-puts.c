591345a2101751dea82bc879b05deb3e2c41e7e9
Bernice30 <maureenotono@gmail.com>
2023-03-27 07:47:21 -0700

    My first c program line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function in a code
+ *
+ * Return: value is always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return 0;
+}
